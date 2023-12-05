eb4794e5ef52c3ad82e46cd1665811373361d4be
Nikhil <118874572+Nikhil690@users.noreply.github.com>
2023-12-05 13:20:23 +0530

    Update NFs.sh

--- NFs.sh
+++ NFs.sh
@@ -1,1 +1,14 @@
-$
+#!/bin/bash
+
+sudo apt update
+
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-amf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-ausf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-nrf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-nssf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-nwdaf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-pcf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-smf.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-udm.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-udr.git -b pre-release-2.0
+git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-upf.git -b pre-release-2.0
