6358b8b005a7521e897281dcea01311082abc225
Nikhil <118874572+Nikhil690@users.noreply.github.com>
2023-12-05 13:23:33 +0530

    Update NFs.sh

--- NFs.sh
+++ NFs.sh
@@ -12,3 +12,44 @@ git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-smf.git -b pre-release-2.0
 git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-udm.git -b pre-release-2.0
 git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-udr.git -b pre-release-2.0
 git clone https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-upf.git -b pre-release-2.0
+
+cd ~/oai-cn5g-amf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-ausf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-nrf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-nssf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-nwdaf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-pcf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-smf
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-udm
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-udr
+git submodule init
+git submodule update
+
+cd ~/oai-cn5g-upf
+git submodule init
+git submodule update
+
